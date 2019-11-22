657cce4f57b502aabf6a90fe48ce2be020105d5e
skykimm <gk5594@naver.com>
2019-11-22 14:11:14 +0900

    new

---
+++ js/index.js
@@ -0,0 +1,13 @@
+var slides = document.querySelector(".slidebox");
+var count = 0;
+
+function slide(){
+    if(count==2){
+        count=0;
+    }
+    else{
+        count++;
+    }
+    slides.style.left= 800*(-count)+"px";
+}
+window.setInterval("slide()",2000);
